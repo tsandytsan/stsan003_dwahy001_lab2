4011082d5ad464074e607cfca7f7bfaefa4002ba
tsandytsan <tsandytsan@gmail.com>
2019-01-08 16:42:13 -0800

    pushing default files

---
+++ stsan003_lab2_part1/stsan003_lab2_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * stsan003_lab2_part1.c
+ *
+ * Created: 1/8/2019 4:37:35 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
